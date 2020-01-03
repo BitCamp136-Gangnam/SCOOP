e064fdf28eb7000680a917c6e70aafd4cc5d01b0
Wonbo <qhdus114@gmail.com>
2020-01-03 10:02:49 +0900

    won

---
+++ Scoop_Project/src/kr/or/scoop/ncontroller/ncontroller.java
@@ -0,0 +1,33 @@
+package kr.or.scoop.ncontroller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ncontroller {
+
+	@RequestMapping("calender.do")
+	public String calender() {
+		return "sidebar/app-calender";
+	}
+
+	@RequestMapping("myissue.do")
+	public String issue() {
+		return "sidebar/app-myissue";
+	}
+
+	@RequestMapping("private.do")
+	public String privateplace() {
+		return "sidebar/private-place";
+	}
+
+	@RequestMapping("index.do")
+	public String main() {
+		return "issue";
+	}
+
+	@RequestMapping("bookmark.do")
+	public String bookmark() {
+		return "sidebar/private-bookmark";
+	}
+}
