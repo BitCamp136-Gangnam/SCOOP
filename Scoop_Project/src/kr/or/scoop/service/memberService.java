a8545913d2932a6448bcb084e0d0279e186c1fcb
ILGYEOM <chdl1229@gmail.com>
2020-01-04 14:32:14 +0900

    null point 오류

--- Scoop_Project/src/kr/or/scoop/service/memberService.java
+++ Scoop_Project/src/kr/or/scoop/service/memberService.java
@@ -1,14 +1,24 @@
 package kr.or.scoop.service;
 
 import org.apache.ibatis.session.SqlSession;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import kr.or.scoop.dao.MemberDao;
+import kr.or.scoop.vo.Member;
 
 @Service
 public class memberService {
 	
-	@Autowired
+
 	private SqlSession sqlsession;
-	
+
+	public int insertMember(Member member){
+		int result = 0;
+		MemberDao dao = sqlsession.getMapper(MemberDao.class);
+		
+		
+		result = dao.insertMember(member);
+		return result;
+	}
 	
 }
