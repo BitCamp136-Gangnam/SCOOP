6cc4ccafac1496c88b4b5df5b3a385dd5dda41a9
ILGYEOM <chdl1229@gmail.com>
2020-01-08 23:48:54 +0900

    서비스 수정 , dao 수정 #다른곳에 오류가 너무심해 작업 불가능

--- SCOOP_Project/src/main/java/kr/or/scoop/service/BoardService.java
+++ SCOOP_Project/src/main/java/kr/or/scoop/service/BoardService.java
@@ -4,15 +4,25 @@ import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import kr.or.scoop.dao.ProjectDao;
+import kr.or.scoop.dto.TeamPjt;
+
 @Service
 public class BoardService {
 	@Autowired
 	private SqlSession sqlsession;
 	
-	public int insertTeamPjt() {
-		return 0;
+	public int insertTeamPjt(TeamPjt team) {
+		int result = 0;
+			ProjectDao dao = sqlsession.getMapper(ProjectDao.class);
+			System.out.println("여기좀 와주라 ㅠㅠㅠㅠ");	
+			
+			result = dao.insertPJT(team);
+			
+		return result;
 		
 	}
+	
 	
 	
 }
