4af6bd90bc640449ae47cf30e8b7d3afb1ec2278
ILGYEOM <chdl1229@gmail.com>
2020-01-13 16:47:10 +0900

    주석

--- SCOOP_Project/src/main/java/kr/or/scoop/dao/BoardDao.java
+++ SCOOP_Project/src/main/java/kr/or/scoop/dao/BoardDao.java
@@ -4,7 +4,7 @@ package kr.or.scoop.dao;
 import kr.or.scoop.dto.Issue;
 
 public interface BoardDao {
-	
+	//이슈 작성
 	public int insertIssue(Issue issue);
 	
 
--- SCOOP_Project/src/main/java/kr/or/scoop/dao/MyIssueDao.java
+++ SCOOP_Project/src/main/java/kr/or/scoop/dao/MyIssueDao.java
@@ -3,5 +3,6 @@ package kr.or.scoop.dao;
 import kr.or.scoop.dto.MyIssue;
 
 public interface MyIssueDao {
+	//나의 이슈 작성
 	public int writeMyissue(MyIssue myissue);
 }
