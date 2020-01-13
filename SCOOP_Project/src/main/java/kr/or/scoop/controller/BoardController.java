e2a31ecbf3786cac1731d426facd55d88a49af61
ILGYEOM <chdl1229@gmail.com>
2020-01-13 12:03:26 +0900

    공지사항 리스트

--- SCOOP_Project/src/main/java/kr/or/scoop/controller/BoardController.java
+++ SCOOP_Project/src/main/java/kr/or/scoop/controller/BoardController.java
@@ -1,15 +1,16 @@
 package kr.or.scoop.controller;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
+import java.util.List;
 
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import kr.or.scoop.dao.NoticeDao;
+import kr.or.scoop.dto.Notice;
 import kr.or.scoop.service.BoardService;
 
 @Controller
@@ -31,5 +32,13 @@ public class BoardController {
 	@RequestMapping(value = "/calendar.do", method = RequestMethod.GET)
 	public String calendar() {
 		return "sidebar/app-calender";
+	}
+
+	@RequestMapping(value="notice.do" , method = RequestMethod.GET)
+	public String noticeJoin(Notice notice, Model model) {
+		NoticeDao dao = sqlSession.getMapper(NoticeDao.class);
+		List<Notice> n = dao.getNotice();
+		model.addAttribute("notice",n);
+		return "issue/notice";
 	}
 }
