031dafa54b1e6afc9e619d0087b5f9acd659549e
doyeon Lim <ldy95091@gamil.com>
2020-01-28 15:03:28 +0900

    캘린더 컨츠로

--- SCOOP_Project/src/main/java/kr/or/scoop/controller/BoardController.java
+++ SCOOP_Project/src/main/java/kr/or/scoop/controller/BoardController.java
@@ -8,7 +8,6 @@ import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -97,12 +96,6 @@ public class BoardController {
 		model.addAttribute("gdrive", googledrive);
 		model.addAttribute("dowork", dowork);
 		return "issue/teamissueDetail";
-	}
-
-	// 캘린더
-	@RequestMapping(value = "/calendar.do", method = RequestMethod.GET)
-	public String calendar() {
-		return "sidebar/app-calender";
 	}
 
 	@RequestMapping(value="notice.do" , method = RequestMethod.GET)
