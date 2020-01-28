fba821ace28430387c99bbf16a707ebe1df32f5e
KimHanNyuck <revubplay@nate.com>
2020-01-28 17:30:20 +0900

    인생 ;

--- SCOOP_Project/src/main/webapp/resources/fullcalendar/main.js
+++ SCOOP_Project/src/main/webapp/resources/fullcalendar/main.js
@@ -166,6 +166,7 @@ var calendar = $('#calendar').fullCalendar({
       },
       success: function (response) {
         var fixedDate = response.map(function (array) {
+        	console.log(response);
           if (array.allDay && array.start !== array.end) {
             // 이틀 이상 AllDay 일정인 경우 달력에 표기시 하루를 더해야 정상출력
             array.end = moment(array.end).add(1, 'days');
