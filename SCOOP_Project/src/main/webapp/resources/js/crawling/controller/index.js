97a98f1434b6b0c0856de4bff07e743fe4abc0b4
Wonbo <qhdus114@gmail.com>
2020-02-01 16:58:26 +0900

    크롤링 테스트

--- SCOOP_Project/src/main/webapp/resources/js/crawling/controller/index.js
+++ SCOOP_Project/src/main/webapp/resources/js/crawling/controller/index.js
@@ -4,7 +4,7 @@ let controller = {
     start: () => {
         console.log('hi')
         let options = {
-            url: 'https://www.naver.com',
+            url: 'https://www.instagram.com/?hl=ko',
             method: 'get',
             timeout: 3000
         }
@@ -34,9 +34,15 @@ let controller = {
         let $ = cheerio.load(body)
         let title = $('head > meta[property="og:title"]').attr('content')
         let image = $('head > meta[property="og:image"]').attr('content')
+        let url = $('head > meta[property="og:url"]').attr('content')
+        let title2 = $('head > title').text()
+        let robot = $('head > meta[name="robots"]').attr('content')
         //$('#PM_ID_ct > .header > .special_bg > .area_flex > .area_logo > h1 > a > span').text()
         console.log(title)
         console.log(image)
+        console.log(url)
+        console.log(title2)
+        console.log(robot)
     }
 }
 
