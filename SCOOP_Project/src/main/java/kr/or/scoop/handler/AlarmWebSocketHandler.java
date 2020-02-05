7de96e380d32c36d6da78242b1a54daa3bc163cb
KimHanNyuck <revubplay@nate.com>
2020-02-05 20:03:46 +0900

    사이다

--- SCOOP_Project/src/main/java/kr/or/scoop/handler/AlarmWebSocketHandler.java
+++ SCOOP_Project/src/main/java/kr/or/scoop/handler/AlarmWebSocketHandler.java
@@ -1,5 +1,18 @@
 package kr.or.scoop.handler;
 
-public class AlarmWebSocketHandler {
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import kr.or.scoop.dto.ChatRoom;
+
+public class AlarmWebSocketHandler extends TextWebSocketHandler {
+
+	// 접속한 전체 유저 관리
+	private Map<String, WebSocketSession> users = new ConcurrentHashMap<>();
+	// 채팅방 관리
+	private Map<String, ChatRoom> roomInfos = new HashMap<>();
 }
